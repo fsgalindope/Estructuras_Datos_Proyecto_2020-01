4d2fa37aee1c8e1f257d11999c2990c396bf32a5
Sebastian <sehernandezce@unal.edu.co>
2020-04-17 18:50:24 -0500

    ok

--- Proyecto_2020-1/src/data/Proyecto_20201.java
+++ Proyecto_2020-1/src/data/Proyecto_20201.java
@@ -1,21 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package data;
 
-/**
- *
- * @author FABIa
- */
 public class Proyecto_20201 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
        System.out.println("sebastian");
-    }
-    
+    }    
 }
