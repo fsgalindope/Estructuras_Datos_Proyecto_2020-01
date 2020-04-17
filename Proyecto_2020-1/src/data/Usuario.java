e6c814e936c2a513d23cd2b1f251d422abb26311
Sebastian <sehernandezce@unal.edu.co>
2020-04-17 18:54:41 -0500

    ok

---
+++ Proyecto_2020-1/src/data/Usuario.java
@@ -0,0 +1,12 @@
+
+package data;
+
+
+public class Usuario {
+    private String nombre;
+    private int id;
+    private String usuario;
+    private String contraseña;
+    private int cuenta;
+    
+}
