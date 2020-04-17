32b66d3a3dd52ea7323e9d4fad15f4bedffd2d14
Sebastian <63028794+sehernandezce@users.noreply.github.com>
2020-04-17 18:12:52 -0500

    1
    1

--- Proyecto_2020-1/src/data/Proyecto_20201.java
+++ Proyecto_2020-1/src/data/Proyecto_20201.java
@@ -15,7 +15,7 @@ public class Proyecto_20201 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("si funciona en internet?");
     }
     
 }
